180734a3eb2fb536ef392b0430f094b1bcc72d52
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2023-02-07 19:41:06 +0200

    BLEN-337: Show required panels for RPRHydra render delegate addon. (#5)
    Added showing required panels.
    Added RPR_HYDRA_LIGHT_PT_light.poll()

--- src/hydrarpr/ui.py
+++ src/hydrarpr/ui.py
@@ -234,6 +234,10 @@ class RPR_HYDRA_LIGHT_PT_light(Panel):
     bl_label = "Light"
     bl_context = 'data'
 
+    @classmethod
+    def poll(cls, context):
+        return super().poll(context) and context.light
+
     def draw(self, context):
         layout = self.layout
 
@@ -278,7 +282,7 @@ class RPR_HYDRA_LIGHT_PT_light(Panel):
                 main_col.prop(light, 'size')
 
 
-register, unregister = bpy.utils.register_classes_factory((
+register_classes, unregister_classes = bpy.utils.register_classes_factory((
     RPR_HYDRA_RENDER_PT_final,
     RPR_HYDRA_RENDER_PT_samples_final,
     RPR_HYDRA_RENDER_PT_quality_final,
@@ -291,6 +295,87 @@ register, unregister = bpy.utils.register_classes_factory((
     RPR_HYDRA_RENDER_PT_quality_viewport,
     RPR_HYDRA_RENDER_PT_denoise_viewport,
     RPR_HYDRA_RENDER_PT_pixel_filter_viewport,
-    
+
     RPR_HYDRA_LIGHT_PT_light,
 ))
+
+
+def get_panels():
+    # follow the Cycles model of excluding panels we don't want
+    exclude_panels = {
+        'DATA_PT_area',
+        'DATA_PT_falloff_curve',
+        'DATA_PT_shadow',
+        'DATA_PT_spot',
+        'DATA_PT_sunsky',
+        'DATA_PT_light',
+        'MATERIAL_PT_diffuse',
+        'MATERIAL_PT_flare',
+        'MATERIAL_PT_halo',
+        'MATERIAL_PT_mirror',
+        'MATERIAL_PT_options',
+        'MATERIAL_PT_pipeline',
+        'MATERIAL_PT_shading',
+        'MATERIAL_PT_shadow',
+        'MATERIAL_PT_specular',
+        'MATERIAL_PT_sss',
+        'MATERIAL_PT_strand',
+        'MATERIAL_PT_transp',
+        'MATERIAL_PT_volume_density',
+        'MATERIAL_PT_volume_integration',
+        'MATERIAL_PT_volume_lighting',
+        'MATERIAL_PT_volume_options',
+        'MATERIAL_PT_volume_shading',
+        'MATERIAL_PT_volume_transp',
+        'RENDERLAYER_PT_layer_options',
+        'RENDERLAYER_PT_layer_passes',
+        'RENDERLAYER_PT_views',
+        'RENDER_PT_antialiasing',
+        'RENDER_PT_bake',
+        'RENDER_PT_motion_blur',
+        'RENDER_PT_performance',
+        'RENDER_PT_freestyle',
+        'RENDER_PT_post_processing',
+        'RENDER_PT_shading',
+        'RENDER_PT_simplify',
+        'RENDER_PT_stamp',
+        'SCENE_PT_simplify',
+        'SCENE_PT_audio',
+        'WORLD_PT_ambient_occlusion',
+        'WORLD_PT_environment_lighting',
+        'WORLD_PT_gather',
+        'WORLD_PT_indirect_lighting',
+        'WORLD_PT_mist',
+        'WORLD_PT_preview',
+        'WORLD_PT_world',
+        'NODE_DATA_PT_light',
+    }
+    include_eevee_panels = {
+        'MATERIAL_PT_preview',
+        'EEVEE_MATERIAL_PT_context_material',
+        'EEVEE_MATERIAL_PT_surface',
+        'EEVEE_MATERIAL_PT_volume',
+        'EEVEE_MATERIAL_PT_settings',
+    }
+
+    for panel_cls in bpy.types.Panel.__subclasses__():
+        if hasattr(panel_cls, 'COMPAT_ENGINES') and (
+                ('BLENDER_RENDER' in panel_cls.COMPAT_ENGINES and panel_cls.__name__ not in exclude_panels) or
+                ('BLENDER_EEVEE' in panel_cls.COMPAT_ENGINES and panel_cls.__name__ in include_eevee_panels)
+        ):
+            yield panel_cls
+
+
+def register():
+    register_classes()
+
+    for panel_cls in get_panels():
+        panel_cls.COMPAT_ENGINES.add(RPRHydraRenderEngine.bl_idname)
+
+
+def unregister():
+    unregister_classes()
+
+    for panel_cls in get_panels():
+        if RPRHydraRenderEngine.bl_idname in panel_cls.COMPAT_ENGINES:
+            panel_cls.COMPAT_ENGINES.remove(RPRHydraRenderEngine.bl_idname)
